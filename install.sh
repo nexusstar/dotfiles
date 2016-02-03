0f1b5b4a3ab4b6affbfbd59b6076c5a493bf990d
Petar Nikov <petar.nikov@gmail.com>
2016-02-03 11:00:34 +0200

    first try on isntall.sh

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+DOTFILES=$HOME/tools/dotfiles
+
+echo "Installing dotfiles"
+
+echo "Initializing submodule(s)"
+git submodule update --init --recursive
+
+if [ "$(uname)" == "Linux" ]; then
+  echo "Running on Linux"
+
+  echo "Installing/Updating nvm"
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.30.2/install.sh | bash
+
+  echo "Installing node with nvm"
+  nvm install stable
+  nvm alias default stable
+
+fi
+
+echo "Installing vim:plug"
+echo -e "\n\ninstalling to ~/tools/dotfiles/vim/.vim"
+echo "=============================="
+if [ ! -d $DOTFILES/vim/.vim/autoload ]; then
+  curl -fLo $DOTFILES/vim/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
