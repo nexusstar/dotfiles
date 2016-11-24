bcaf455cd87892d2fd446810790ffe530e330d81
Petar Nikov <petar.nikov@gmail.com>
2016-11-24 16:06:47 +0200

    ZSH history completion and remove unused functions

--- zsh/.config/zshenv/config.zsh
+++ zsh/.config/zshenv/config.zsh
@@ -21,6 +21,24 @@ setopt APPEND_HISTORY
 
 setopt COMPLETE_ALIASES
 
+function h() {
+    if [ -z "$*" ];
+        then history 1;
+    else
+        history 1 | egrep "$@";
+    fi;
+}
+
+# zsh history completion from last entered
+autoload -Uz up-line-or-beginning-search
+autoload -Uz down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+bindkey '\eOA' up-line-or-beginning-search
+bindkey '\e[A' up-line-or-beginning-search
+bindkey '\eOB' down-line-or-beginning-search
+bindkey '\e[B' down-line-or-beginning-search
+
 # make terminal command navigation sane again
 bindkey '^[^[[D' backward-word
 bindkey '^[^[[C' forward-word
--- zsh/.config/zshenv/functions.zsh
+++ zsh/.config/zshenv/functions.zsh
@@ -32,13 +32,6 @@ function ng-stop() {
     sudo launchctl stop homebrew.mxcl.nginx
 }
 
-function ng-start() {
-    sudo launchctl start homebrew.mxcl.nginx
-}
-function ng-restart() {
-     sudo launchctl start homebrew.mxcl.nginx
-}
-
 
 # Start an HTTP server from a directory, optionally specifying the port
 function server() {
@@ -103,12 +96,6 @@ function extract() {
     fi
 }
 
-function scpp() {
-    scp "$1" nicknisi@nicknisi.com:/var/www/nicknisi.com/public_html/share;
-    echo "http://nicknisi.com/share/$1" | pbcopy;
-    echo "Copied to clipboard: http://nicknisi.com/share/$1"
-}
-
 # syntax highlight the contents of a file or the clipboard and place the result on the clipboard
 function hl() {
     if [ -z "$3" ]; then
@@ -124,13 +111,4 @@ function hl() {
     fi
 
     echo $src | highlight -O rtf --syntax $1 --font Inconsoloata --style $style --line-number --font-size 24 | pbcopy
-}
-
-# set the background color to light
-function light() {
-    export BACKGROUND="light" && reload!
-}
-
-function dark() {
-    export BACKGROUND="dark" && reload!
 }
