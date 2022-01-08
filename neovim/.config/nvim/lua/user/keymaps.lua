0711c0a8e90a81107dea50ca8ab01d841846ec3c
Petar Nikov <petar.nikov@gmail.com>
2022-01-09 00:53:17 +0200

    Nvim/lua - add to keybindings.

--- neovim/.config/nvim/lua/user/keymaps.lua
+++ neovim/.config/nvim/lua/user/keymaps.lua
@@ -48,6 +48,9 @@ keymap("n", "<CR>", ":nohlsearch<CR>/<BS>", opts)
 -- Corrects the spelling under the cursor with first suggestion
 keymap("n", "<S-z>", "1z=", {silent = true}) -- FIXME Does not work
 
+-- Default formatting, and keep the cursor in place
+keymap("n", "Q", "mzgg=G`z", opts)
+
 -- Insert --
 -- Press jk fast to exit insert mode
 keymap("i", "jk", "<ESC>", opts)
@@ -56,6 +59,9 @@ keymap("i", "jk", "<ESC>", opts)
 keymap("i", "<C-u>", "<Esc>mzgUiw`za", opts)
 
 -- Visual --
+-- Default formatting, and keep the cursor in place
+keymap("v", "Q", "mz=`z", opts)
+
 -- Stay in indent mode
 keymap("v", "<", "<gv", opts)
 keymap("v", ">", ">gv", opts)
