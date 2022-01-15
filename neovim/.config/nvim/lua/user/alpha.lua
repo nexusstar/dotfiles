05e33e0a5bf437b0298ee39b8907b8d6376b5665
Petar Nikov <petar.nikov@gmail.com>
2022-01-15 10:28:40 +0200

    Nvim: lua config update.

---
+++ neovim/.config/nvim/lua/user/alpha.lua
@@ -0,0 +1,39 @@
+local status_ok, alpha = pcall(require, "alpha")
+if not status_ok then
+	return
+end
+
+local kind = require "user.lsp_kind"
+local dashboard = require("alpha.themes.dashboard")
+local thingy = io.popen 'echo "$(date +%a) $(date +%d) $(date +%b)" | tr -d "\n"'
+local date = thingy:read "*a"
+thingy:close()
+dashboard.section.header.val = "╭────── " .. kind.icons.calendar .. " Today is " .. date .. " ───────┐"
+dashboard.section.buttons.val = {
+	dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
+	dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+	dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
+	dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
+	dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
+	dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua <CR>"),
+	dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+}
+
+local function footer()
+-- NOTE: requires the fortune-mod package to work
+	local handle = io.popen("fortune")
+  local fortune = require "alpha.fortune"()
+	--local fortune = handle:read("*a")
+	handle:close()
+	return fortune
+end
+
+dashboard.section.footer.val = footer()
+
+dashboard.section.footer.opts.hl = "Type"
+dashboard.section.header.opts.hl = "Include"
+dashboard.section.buttons.opts.hl = "Keyword"
+
+dashboard.opts.opts.noautocmd = true
+-- vim.cmd([[autocmd User AlphaReady echo 'ready']])
+alpha.setup(dashboard.opts)
