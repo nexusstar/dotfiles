5f345067723ecb265b6b34d4620c485e3ce72a34
Petar Nikov <petar.nikov@gmail.com>
2022-01-16 09:33:19 +0200

    Nvim: lua customize lualine.

--- neovim/.config/nvim/lua/user/lualine.lua
+++ neovim/.config/nvim/lua/user/lualine.lua
@@ -2,7 +2,7 @@ local status_ok, lualine = pcall(require, "lualine")
 if not status_ok then
 	return
 end
-
+local kind = require "user.lsp_kind"
 local hide_in_width = function()
 	return vim.fn.winwidth(0) > 80
 end
@@ -60,6 +60,49 @@ end
 
 local spaces = function()
 	return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
+end
+local mode = function()
+  local mod = vim.fn.mode()
+  local _time = os.date "*t"
+  local selector = math.floor(_time.hour / 8) + 1
+  local normal_icons = {
+    "  ",
+    "  ",
+    "  ",
+  }
+  if mod == "n" or mod == "no" or mod == "nov" then
+    return normal_icons[selector]
+  elseif mod == "i" or mod == "ic" or mod == "ix" then
+    local insert_icons = {
+      "  ",
+      "  ",
+      "  ",
+    }
+    return insert_icons[selector]
+  elseif mod == "V" or mod == "v" or mod == "vs" or mod == "Vs" or mod == "cv" then
+    local verbose_icons = {
+      " 勇",
+      "  ",
+      "  ",
+    }
+    return verbose_icons[selector]
+  elseif mod == "c" or mod == "ce" then
+    local command_icons = {
+      "  ",
+      "  ",
+      "  ",
+    }
+
+    return command_icons[selector]
+  elseif mod == "r" or mod == "rm" or mod == "r?" or mod == "R" or mod == "Rc" or mod == "Rv" or mod == "Rv" then
+    local replace_icons = {
+      "  ",
+      "  ",
+      "  ",
+    }
+    return replace_icons[selector]
+  end
+  return normal_icons[selector]
 end
 
 lualine.setup({
