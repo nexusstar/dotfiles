d2bb3a38617531cb7639f02d3ba01948dfa29c3e
Petar Nikov <petar.nikov@gmail.com>
2022-01-20 17:54:42 +0200

    Nvim: null-ls update.

--- neovim/.config/nvim/lua/user/lsp/null-ls.lua
+++ neovim/.config/nvim/lua/user/lsp/null-ls.lua
@@ -1,19 +1,47 @@
-local null_ls_status_ok, null_ls = pcall(require, "null-ls")
+local null_ls_status_ok, nls = pcall(require, "null-ls")
 if not null_ls_status_ok then
 	return
 end
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
-local formatting = null_ls.builtins.formatting
+local formatting = nls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
-local diagnostics = null_ls.builtins.diagnostics
+local diagnostics = nls.builtins.diagnostics
+--https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/code_actions 
+local code_actions = nls.builtins.code_actions
 
-null_ls.setup({
+nls.setup({
 	debug = false,
+  debounce = 150,
 	sources = {
-		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
+    formatting.prettierd.with {
+      condition = function(utils)
+        return not utils.root_has_file { ".eslintrc", ".eslintrc.js" }
+      end,
+      prefer_local = "node_modules/.bin",
+    },
+    formatting.eslint_d.with {
+      condition = function(utils)
+        return utils.root_has_file { ".eslintrc", ".eslintrc.js" }
+      end,
+      prefer_local = "node_modules/.bin",
+    },
 		formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.stylua,
-    -- diagnostics.flake8
+    -- Diagnostics
+    diagnostics.eslint_d.with {
+      condition = function(utils)
+        return utils.root_has_file { ".eslintrc", ".eslintrc.js" }
+      end,
+      prefer_local = "node_modules/.bin",
+    },
+    diagnostics.luacheck,
+    -- Code actions
+    code_actions.eslint_d.with {
+      condition = function(utils)
+        return utils.root_has_file { ".eslintrc", ".eslintrc.js" }
+      end,
+      prefer_local = "node_modules/.bin",
+    },
 	},
 })
