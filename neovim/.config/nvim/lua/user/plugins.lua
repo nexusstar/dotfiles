b5ea88fad122a7546a45df718dcb3f0ef837c631
Petar Nikov <petar.nikov@gmail.com>
2022-09-05 10:14:00 +0300

    feat: Nvim add surround

--- neovim/.config/nvim/lua/user/plugins.lua
+++ neovim/.config/nvim/lua/user/plugins.lua
@@ -158,7 +158,16 @@ return packer.startup(function(use)
   -- Grammar checker
   -- use "rhysd/vim-grammarous"
   -- [[ use "mustache/vim-mustache-handlebars" ]]
-
+  -- Surround
+  use({
+    "kylechui/nvim-surround",
+    tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end
+  })
   -- Automatically set up configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
